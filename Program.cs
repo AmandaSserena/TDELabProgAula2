d3b5dabfd5981eacfd79d5b1f454417421292942
Amanda Serena <amandaserena@rede.ulbra.br>
2024-03-16 17:28:35 -0300

    first init

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//Trabalho de TDE aula2
+//1-Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+int idade = 35;
+Console.WriteLine(idade);
+
+//2-Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+
+string nome = "Maria";
+Console.WriteLine(nome);
+
+//3-Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+float altura = 3.45f;
+Console.WriteLine(altura);
+
+//4-Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+
+const int ano = 12;
+Console.WriteLine(ano);
+
+//5-Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o valor 7.80 e exiba o seu valor
+
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+//6-Quais as diferenças entre os tipos por valor e os tipos por referência ?
+
+//Os tipos por valor são armazenados na pilha e os tipos por referência são armazenados no heap.
+
+//7-O que é um nullable type e qual a sua utilidade ?
+
+//Nullable type é um tipo que pode receber valores nulos, sua utilidade é para quando não se tem certeza se a variável vai receber um valor ou não.
+
+//8-O que é Camel Case ? Dê um exemplo de sua aplicação.
+
+//Camel Case é uma convenção de nomenclatura que consiste em escrever a primeira palavra com letra minúscula e as demais palavras com a primeira letra maiúscula. Exemplo: nomeCompleto.
+
+//9-O que é Pascal Case ? Dê um exemplo de sua aplicação
+
+//Pascal Case é uma convenção de nomenclatura que consiste em escrever todas as palavras com a primeira letra maiúscula. Exemplo: NomeCompleto.
+
+//10-Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e aseguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar aoperação de soma)
+
+int x = 77;
+int y = 66;
+Console.WriteLine(x + y);
+
+//11-Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e string.
+
+//bool = false
+//char = '\0'
+//int = 0
+//double = 0.0
+//float = 0.0f
+//decimal = 0.0m
+//string = null
